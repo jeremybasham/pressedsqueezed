19dd6f7c2459d81fe0eacb9f2c7007d3b9594d80
Jeremy Basham <jbasham@mcad.edu>
2013-02-17 20:26:23 -0500

    PressedSqueezed with Underscore theme
    Creating a theme and modifying functions, setting type, removing
    comments, and modifying footer.

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package pressedsqueezed
+ * @since pressedsqueezed 1.0
+ */
+?>
+
+	</div><!-- #main .site-main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+	
+        <h1>Contact Us</h1>
+		<p>Orange Peel</p>
+		<p>2525 Orange Way, Orchard, CA 91104</p>
+		<p>Phone: (800) 424-3232</p>
+  
+		
+	</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
